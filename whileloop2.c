66a5535ed9c4c0cb16a16297bade3c3710baeff2
Judah Gnanam <judahgnanam@judahs-mbp.home>
2020-11-05 18:55:02 -0600

    UNFINISHED AND BAD PROGRAM FIX OR...

---
+++ whileloop2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+write a program to print all even numbers
+given the highest even number entered by the user
+*/
+
+int main()
+{
+    int even_max;
+    int even_numbers;
+
+    printf("Please enter the higest even number");
+    scanf("%d\n", &even_max);
+
+    while (even_max = even_numbers)
+    {
+
+        
+
+    }
+}
